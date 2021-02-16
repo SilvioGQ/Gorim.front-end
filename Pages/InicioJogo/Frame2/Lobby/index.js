1e2128a4f826953644481756064a650a7206b078
SilvioGQ <silvioquintana1@hotmail.com>
2021-02-15 21:06:39 -0300

    Arrumar erro

--- Pages/InicioJogo/Frame2/Lobby/index.js
+++ Pages/InicioJogo/Frame2/Lobby/index.js
@@ -4,7 +4,6 @@ import { Text, View, StyleSheet, Dimensions, FlatList } from 'react-native';
 
 import Button from '../../../../Components/Button';
 import PlayerService from '../../../../firebase/services/PlayerService';
-import { db } from '../../../../firebase';
 
 const Tela = Dimensions.get('screen').width;
 export default function Lobby({ navigation, route }) {
