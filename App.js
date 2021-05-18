bd8b9e64b5e449d49e481359989165f9901e0ed5
Alan Santos <alanssantos32@gmail.com>
2021-05-18 13:54:02 -0300

    Update dependencies

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import 'react-native-gesture-handler';
 import React from 'react';
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { AppLoading } from 'expo';
+import AppLoading from 'expo-app-loading';
 import Routes from './Pages/Routes';
 import { useFonts, Rubik_300Light, Rubik_400Regular, Rubik_700Bold } from '@expo-google-fonts/rubik';
 
