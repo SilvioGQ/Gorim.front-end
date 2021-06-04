8f58af720fe9ac53227183e92025a23817d0e9fb
Alan Santos <alanssantos32@gmail.com>
2021-06-04 12:48:33 -0300

    Add analysis with socket

--- Pages/Analisar/index.js
+++ Pages/Analisar/index.js
@@ -1,23 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Text, View, StyleSheet, Image, TouchableOpacity, Dimensions, FlatList } from 'react-native';
+import { socketContext } from '../../context/socket';
 import COLORS from '../../resources/colors';
 
 import Produtos from '../../Components/Produtos';
 import ModalInfo from '../../Components/ModalInfo';
 
-import FunctionaliryService from '../../services/FunctionalityService';
+// import FunctionaliryService from '../../services/FunctionalityService';
 import { StatusBar } from 'react-native';
 
 const Tela = Dimensions.get('screen').width;
 export default function Analizar() {
+
   const [modalText, setModalText] = useState('');
   const [modalImage, setModalImage] = useState(true);
   const [products, setProducts] = useState([]);
-  const textInfo = 'Informações em tela: \nIcones e nomes dos produtos que podem ser usados nas parcelas de terra';
+  const socket = useContext(socketContext);
 
   useEffect(() => {
     // setModalImage(true);
-    FunctionaliryService.getProducts().then(setProducts);
+    // FunctionaliryService.getProducts().then(setProducts);
     socket.emit('getProducts', resp => setProducts(resp));
   }, []);
 
@@ -25,11 +27,11 @@ export default function Analizar() {
     <View style={styles.container}>
       <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: '10%'}}>
         <Text style={styles.header}>Produtos</Text>
-        <TouchableOpacity onPress={() => setModalText(textInfo)}>
+        <TouchableOpacity onPress={() => setModalText('Informações em tela: \nIcones e nomes dos produtos que podem ser usados nas parcelas de terra')}>
           <Image source={require('../../assets/agricultorIcones/information.png')} style={{ width: 20, height: 20, marginVertical: 5, marginLeft: 10 }} />
         </TouchableOpacity>
       </View>
-      {modalText !== '' && <ModalInfo onClick={() => setModalText('')} text={modalText} modalImage ={modalImage}/>}
+      {modalText !== '' && <ModalInfo onClick={() => setModalText('')} text={modalText} modalImage={modalImage}/>}
       <FlatList
         showsVerticalScrollIndicator={false}
         data={products}
