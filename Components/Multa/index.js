976920cd2ce839b7626716a37e8ddc75d0ef1ff4
SilvioGQ <silvioquintana1@hotmail.com>
2021-09-24 19:10:11 -0300

    Upado

--- Components/Multa/index.js
+++ Components/Multa/index.js
@@ -1,27 +1,42 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import Dropdown from 'react-native-dropdown-enhanced';
 import COLORS from '../../constants/colors';
-import IMAGES from '../../constants/imagesProducts';
-import Modal from '../../../Components/ModalInfo'
-export default function Multa({ item, player }) {
+import IMAGES from '../../constants/imagesIcons';
+import Modal from '../../Components/ModalInfo'
+export default function MultaComponent({ item, player }) {
     const [modalText, setModalText] = useState('');
     const information = () => {
         return setModalText('Tabela para aplicação de multas.\nO porra.');
-    }
+    };
+    const data= [
+        {label: 'Baixo', value: 1},
+        {label: 'Medio', value: 2},
+        {label: 'Alto', value: 3},
+        {label: 'Nenhum', value: 4},
+    ]
     return (
         <View style={styles.container}>
             <View>
-                <Image style={styles.icone} source={IMAGES[player.avatar]} />
-                <Text style={styles.textinhos}>{player.name}</Text>
+                <Image style={styles.icone} source={IMAGES['Icon3']} />
+                <Text style={styles.textinhos}>{'nome kjdsklfa'}</Text>
             </View>
             <View>
-                <Text>Total poluição</Text>
-                <Text>Gravidade: </Text>
+                <Text style={styles.text}>Total poluição</Text>
+                <View style={{flexDirection:'row'}}>
+                <Text style={styles.text}>Gravidade: </Text>
+                <Dropdown
+                    data={data}
+                    style={{height:28, width:110, borderRadius:17}}
+                    defaultValue={2}
+                    onSelectedChange={({ label }) => console.log(label)}
+                />
+                </View>
                 <View style={styles.row}>
-                    <TouchableOpacity style={styles.button} onPress={() => {}} activeOpacity={0.7}>
+                    <TouchableOpacity style={styles.button} onPress={() => { }} activeOpacity={0.7}>
                         <Text style={styles.textbutton}>CONFIRMAR</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={[styles.button,{backgroundColor:'#2D7830'}]} onPress={() =>{}} activeOpacity={0.7}>
+                    <TouchableOpacity style={[styles.button, { backgroundColor: '#2D7830' }]} onPress={() => { }} activeOpacity={0.7}>
                         <Text style={styles.textbutton}>VER MAIS</Text>
                     </TouchableOpacity>
                 </View>
@@ -38,11 +53,26 @@ const styles = StyleSheet.create({
         padding: 15,
         borderRadius: 17,
         marginTop: 15,
-        marginBottom: 4
+        marginBottom: 4,
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 4,
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+        elevation: 6,
+        marginHorizontal:20
     },
-    numero: {
-        fontSize: 14,
-        fontFamily: 'Rubik_300Light'
+    text: {
+        fontSize: 16,
+        fontFamily: 'Rubik_300Light',
+        alignItems:'center'
+    },
+    textinhos: {
+        fontSize: 13,
+        fontFamily: 'Rubik_300Light',
+        textAlign:'center'
     },
     row: {
         flexDirection: 'row',
@@ -52,16 +82,22 @@ const styles = StyleSheet.create({
         width: 62,
         height: 60,
     },
+    icone: {
+        width: 52,
+        height: 52,
+        marginTop:15,
+        marginHorizontal:10
+    },
     button: {
-        width: 120,
+        width: 108,
         borderRadius: 20,
         backgroundColor: '#66BF00',
         padding: 12
-      },
-      textbutton: {
+    },
+    textbutton: {
         color: COLORS.textWhite,
         fontSize: 12,
         textAlign: 'center',
         fontFamily: 'Rubik_400Regular'
-      },
+    },
 });
--- Pages/Fiscal/Multa/index.js
+++ Pages/Fiscal/Multa/index.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Image, ScrollView, TouchableOpacity, Dimensions, TouchableWithoutFeedback } from 'react-native';
+import { Text, View, StyleSheet, Image, ScrollView, TouchableOpacity, Dimensions } from 'react-native';
 import Button from '../../../Components/Button';
 import Rodada from '../../../Components/Rodada';
-
+import MultaComponent from '../../../Components/Multa';
 const Tela = Dimensions.get('screen').width;
 export default function Multa({ navigation }) {
-  const [activated, setActivated] = useState(false)
-  const [activated2, setActivated2] = useState(false)
-
+  const [modalText, setModalText] = useState('');
   return (
     <View style={styles.container}>
       <Rodada name={'Multa'} arrow={true} onClick={() => navigation.navigate('MenuPolitico')} />
@@ -19,12 +17,12 @@ export default function Multa({ navigation }) {
         {modalText !== '' && (
           <Modal onClick={() => setModalText('')} text={modalText} />
         )}
-        <Text style={styles.title}>Aplicação{'\n'}de multa</Text>
-        <TouchableOpacity onPress={information} activeOpacity={0.7}>
+        <Text style={styles.header}>Aplicação{'\n'}de multa</Text>
+        <TouchableOpacity onPress={()=>{}} activeOpacity={0.7}>
           <Image source={require('../../../assets/agricultorIcones/information.png')} style={{ width: 20, height: 20, alignSelf: 'center', marginLeft: 10, marginTop: 20 }} />
         </TouchableOpacity>
       </View>
-      
+      <MultaComponent/>
       <Button
         onClick={() => navigation.navigate('Fiscal')}
         name='APLICAR' />
@@ -35,95 +33,28 @@ export default function Multa({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: Tela
+    width: Tela,
+    alignItems:'center',
+    justifyContent:'center'
   },
   row: {
     flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    marginTop: 20,
-    marginLeft: 8,
-    width: Tela,
-    flexWrap: 'wrap'
-  },
-  espaco: {
-    flexDirection: 'row',
-    alignSelf: 'center',
-    justifyContent: 'center',
-    margin: 15,
-    width: Tela
-  },
-  row3: {
-    flexDirection: 'row',
-    alignSelf: 'center',
-    justifyContent: 'flex-end',
-    margin: 10,
-    width: Tela
-  },
-  row2: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    marginTop: 10,
-    marginLeft: 6
-  },
-  colunm: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 10,
-    backgroundColor: '#FFFFFF',
-    width: 96,
-    height: 84,
-    borderRadius: 20,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.32,
-    shadowRadius: 5.46,
-
-    elevation: 9
+    marginVertical: 20,
+    flexWrap: 'wrap',
+    alignItems:'center',
+    justifyContent:'center'
   },
   header: {
     fontFamily: 'Rubik_300Light',
-
     fontSize: 20,
   },
-  textinhos: {
-    fontFamily: 'Rubik_300Light',
-    fontSize: 14,
-  },
-  logo: {
-    width: 20,
-    height: 23
-  },
-  icone: {
-    width: 32,
-    height: 35,
+  image: {
+    width: 60,
+    height: 60,
   },
   font: {
     fontSize: 18,
     marginTop: 15,
     fontFamily: 'Rubik_300Light'
   },
-  linha: {
-    margin: '8%',
-    marginLeft: '5%',
-    borderWidth: 1,
-    width: '85%',
-    height: 1
-  },
-  nivel: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginRight: '5%',
-    marginTop: -10
-  },
-  traco: {
-    width: 1,
-    height: 12,
-    zIndex: 1,
-    borderWidth: 1,
-  }
 });
