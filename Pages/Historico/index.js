8df88ba5224e3bca83f31b5ed3dda18c5d063382
SilvioGQ <silvioquintana1@hotmail.com>
2021-09-29 15:27:29 -0300

    Upado

--- Pages/Historico/index.js
+++ Pages/Historico/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Text, View, StyleSheet, Image, Dimensions, ScrollView, TouchableOpacity } from 'react-native';
 
 import { GameContext } from '../../contexts/GameContext';
@@ -13,6 +13,7 @@ export default function Cenario({ navigation }) {
   const [open3, setOpen3] = useState(false);
   const [open4, setOpen4] = useState(false);
   const [open5, setOpen5] = useState(false);
+  const [Logs, setLogs] = useState();
   const { player, logs, round } = useContext(GameContext);
   const rotateZ = open ? "180deg" : "0deg";
   const rotateZ2 = open2 ? "180deg" : "0deg";
@@ -20,6 +21,9 @@ export default function Cenario({ navigation }) {
   const rotateZ4 = open4 ? "180deg" : "0deg";
   const rotateZ5 = open5 ? "180deg" : "0deg";
   console.log(logs);
+  useEffect(()=>{
+    setLogs(logs.find((p)=> p.id === player.id))
+  }, [])
   return (
       <ScrollView>
     <View>
@@ -50,7 +54,7 @@ export default function Cenario({ navigation }) {
                 </TouchableOpacity>
               </View>
               <View style={{ display: open ? 'flex' : 'none', flexDirection: 'column' }}>
-                {logs.filter((item) =>item.type == 'plantation').length > 0 ? logs.filter((item) =>item.type == 'plantation').map((item, index) => {
+                {Logs.logs.filter((item) =>item.type == 'plantation').length > 0 ? Logs.logs.filter((item) =>item.type == 'plantation').map((item, index) => {
                   return <Text style={{ marginLeft: 5, paddingHorizontal:5, fontFamily: 'Rubik_300Light' }} key={index}>Parcela {item.parcelLand.id + 1}{'\n'}Semente: {item.parcelLand.seed},{item.parcelLand.pesticide ? ` Agrotóxico: ${item.parcelLand.pesticide.replace(/Agrotóxico /, '')},` : ''}{item.parcelLand.fertilizer ? ` Fertilizante: ${item.parcelLand.fertilizer.replace(/Fertilizante /, '')},` : ''}{item.parcelLand.machine ? ` Máquina: ${item.parcelLand.machine},` : ''} Pulverizador:{item.parcelLand.spray ? ' Sim' : ' Não'}{'\n'}</Text>
                 })
                 :
@@ -71,7 +75,7 @@ export default function Cenario({ navigation }) {
               </TouchableOpacity>
             </View>
             <View style={{ display: open2 ? 'flex' : 'none' }}>
-              {logs.filter((item) =>item.type == 'buy').length > 0 ? logs.filter((item) =>item.type == 'buy').map((item, index) => {
+              {Logs.logs.filter((item)=>item.type == 'buy').length > 0 ? Logs.logs.filter((item)=>item.type == 'buy').map((item, index) => {
                 return <Text style={{ marginLeft: 5, paddingHorizontal:5, fontFamily: 'Rubik_300Light' }} key={index}>{item.ownAction ? `Você comprou ${item.product.amount} ${item.product.name} por $${item.product.price} cada, do empresário ${item.namePlayer} \n` : `Você vendeu ${item.product.amount} ${item.product.name} por $${item.product.price} cada, para o agricultor ${item.namePlayer} \n`}</Text>
               })
               :
@@ -93,7 +97,7 @@ export default function Cenario({ navigation }) {
               </TouchableOpacity>
             </View>
             <View style={{ display: open3 ? 'flex' : 'none' }}>
-              {logs.filter((item) =>item.type == 'transfer').length > 0 ? logs.filter((item) =>item.type == 'transfer').map((item, index) => {
+              {Logs.logs.filter((item) =>item.type == 'transfer').length > 0 ? Logs.logs.filter((item) =>item.type == 'transfer').map((item, index) => {
                 return <Text style={{ marginLeft: 5, paddingHorizontal:5, fontFamily: 'Rubik_300Light' }} key={index}>{item.ownAction ? `Você transferiu $${item.value} para o jogador ${item.namePlayer}\n` : `Você recebeu $${item.value} do jogador ${item.namePlayer}\n`}</Text>
               })
               :
@@ -123,7 +127,7 @@ export default function Cenario({ navigation }) {
                 <Image style={{ width: 35, height: 35, marginRight: 10, marginTop: 5, transform: [{ rotateZ: rotateZ5 }] }} source={require('../../assets/dropdown.png')} />
               </TouchableOpacity>
             </View>
-            <View style={{ display: open5 ? 'flex' : 'none' }}>{logs.filter((item) => {
+            <View style={{ display: open5 ? 'flex' : 'none' }}>{Logs.logs.filter((item)=> {
               if (item.type == 'tax') {
                 return item
               }
