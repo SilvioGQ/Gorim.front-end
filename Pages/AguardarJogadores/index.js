cd0587a9bfd61d6ce713ccd35c4d16ecbc16c2cb
mmarifernandes <marinafernandess103@gmail.com>
2021-11-25 12:03:00 -0300

    sair do aguardando jogadores

--- Pages/AguardarJogadores/index.js
+++ Pages/AguardarJogadores/index.js
@@ -1,16 +1,23 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Text, View, StyleSheet, Image, StatusBar } from 'react-native';
 import { GameContext, removeToRoom, } from '../../contexts/GameContext';
 import Rodada from '../../Components/Rodada';
+import ModalConfirmExit from '../../Components/ModalConfirmExit';
+import Modal from '../../Components/ModalInfo';
+import ModalAsk from '../../Components/ModalAsk';
 
 export default function AguardarJogadores({ navigation }) {
-
+  const [modalText, setModalText] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalVisible2, setModalVisible2] = useState(false);
   const { players, player, awaitPlayers, stage } = useContext(GameContext);
 
   useEffect(() => {
     let isMounted = true;
     if (stage === 'ALLFORENDSTAGE' && isMounted) navigation.reset({ routes: [{ name: 'Status' }] });
     if (stage === 'NEXTROUND' && isMounted) navigation.reset({ routes: [{ name: 'MenuJogador' }] });
+    if (stage === 'REMOVEDTOROOM' && isMounted) navigation.reset({ routes: [{ name: 'Gorim' }] });
+
     return () => isMounted = false;
   }, [stage]);
 
@@ -22,7 +29,12 @@ export default function AguardarJogadores({ navigation }) {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor={'#58AB23'} StatusBarStyle='light-content' />
-      <Rodada removeFromRoom={removeFromRoom} close={true} name={'Aguardando jogadores'} />
+      <Rodada removeFromRoom={removeFromRoom} close={true} name={'Aguardando jogadores'} setModalVisible={setModalVisible} />
+      {modalVisible && <ModalConfirmExit deletePlayer={removeFromRoom} onClick={() => setModalVisible(!modalVisible)} />}
+      {modalVisible2 && <ModalAsk finish={() => { endStage(); stopCallback(); }} back={() => setModalVisible2(!modalVisible2)} />}
+      {modalText !== '' && (
+        <Modal onClick={() => setModalText('')} text={modalText} />
+      )}
       <View style={styles.container2}>
         <Image style={styles.logo} source={require('../../assets/symbols/clock.png')} />
         <Text style={styles.texto}> Aguardando {'\n'} os outros jogadores...</Text>
