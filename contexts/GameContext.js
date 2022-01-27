4b7fe6296aa0febe11622bc0247687b8a533597c
SilvioGQ <silvioquintana1@hotmail.com>
2022-01-27 15:48:26 -0300

    upado

--- contexts/GameContext.js
+++ contexts/GameContext.js
@@ -91,7 +91,7 @@ const GameProvider = (props) => {
     socket.on('reportMessage', (msg) => {
       // removedToRoom, maxPlayersToRoom, inGaming, raffled, notFound, selectedAvatars, allForEndStage, allForEndRound, initElections
       dispatch({ type: msg.toUpperCase(), payload: msg.toUpperCase() });
-      if (msg === 'selectedAvatars') startTimer(20, () => endStage());
+      if (msg === 'selectedAvatars') startTimer(400, () => endStage());
       if (msg === 'INITELECTIONS') startTimer(20, () => addCandidature(null));
       if (msg === 'INITVOTATION') startTimer(20, () => addVote({ mayor: '', cityCouncilor: '', supervisor: '' }));
       if (msg === 'INITRESULTSVOTATION') startTimer(20, () => nextStage());
@@ -151,12 +151,12 @@ const GameProvider = (props) => {
 
     socket.on('nextStage', () => {
       dispatch({ type: 'NEXTSTAGE', payload: 'NEXTSTAGE' });
-      startTimer(20, () => endRound());
+      startTimer(400, () => endRound());
     });
 
     socket.on('nextRound', () => {
       dispatch({ type: 'NEXTROUND', payload: 'NEXTROUND' });
-      startTimer(20, () => endStage());
+      startTimer(400, () => endStage());
     });
 
     // socket.on('reconnectToRoom', (stage) => {
